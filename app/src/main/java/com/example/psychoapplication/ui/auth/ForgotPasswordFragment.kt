39084166a6377b2d89f8aa0371f95a8cfcf7d3ba
Khen Danil <davas123@mail.ru>
2023-05-05 15:52:54 +0700

    add forgot password

---
+++ app/src/main/java/com/example/psychoapplication/ui/auth/ForgotPasswordFragment.kt
@@ -0,0 +1,72 @@
+package com.example.psychoapplication.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.psychoapplication.R
+import com.example.psychoapplication.databinding.FragmentForgotPasswordBinding
+import com.example.psychoapplication.databinding.FragmentLoginBinding
+import com.example.psychoapplication.util.UiState
+import com.example.psychoapplication.util.hide
+import com.example.psychoapplication.util.show
+import com.example.psychoapplication.util.toast
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ForgotPasswordFragment : Fragment() {
+
+    private var _binding: FragmentForgotPasswordBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: AuthViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observer()
+        binding.sendBtn.setOnClickListener{
+            viewModel.forgotPassword(binding.emailEt.text.toString())
+        }
+        binding.back.setOnClickListener{
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun observer(){
+        viewModel.forgotPassword.observe(viewLifecycleOwner) { state ->
+            when(state){
+                is UiState.Loading -> {
+                    binding.sendBtn.text = ""
+                    binding.sendProgress.show()
+                }
+                is UiState.Failure -> {
+                    binding.sendBtn.text = "Login"
+                    binding.sendProgress.hide()
+                    toast(state.error)
+                }
+                is UiState.Success -> {
+                    binding.sendBtn.text = "Login"
+                    binding.sendProgress.hide()
+                    toast(state.data)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
